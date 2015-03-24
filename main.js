a58c2ff0c83bbc2497ed34c7766c7bab23c1e7d8
James Lippitt <james.lippitt@wjweb.co.uk>
2015-03-24 11:26:33 +0000

    Fix missing 'main' in package.json

---
+++ main.js
@@ -0,0 +1,7 @@
+var netacuity = require('./src/netacuity');
+var NetAcuityCache = require('./src/netacuity-cache');
+
+module.exports = {
+    NetAcuity: netacuity.NetAcuity,
+    NetAcuityCache: NetAcuityCache
+};
